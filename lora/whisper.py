d15e5be6ab3ce02b517d3a74341eb99b074760ef
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2025-11-13 21:34:30 +0800

    Delete unused file

--- lora/whisper.py
+++
@@ -1,6 +0,0 @@
-# Download youtube video as mp4
-# Save to content/videoname.mp4
-# !yt-dlp https://youtu.be/H0iHi7iFkiQ --format m4a -o "./content/%(id)s.%(ext)s"
-
-# Use whisper model to transcribe
-# !whisper "./content/H0iHi7iFkiQ.m4a" --model small --language English
